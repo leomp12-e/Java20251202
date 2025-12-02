90e94328ba52c5d5b5b6dd9de74ff6669049f004
Leo Menéndez Perdomo <leomp12@educastur.es>
2025-12-02 18:09:19 +0100

    Actualización

--- src/main/java/fp/dam/java/Main.java
+++ src/main/java/fp/dam/java/Main.java
@@ -1,7 +1,48 @@
 package fp.dam.java;
 
 import java.util.ArrayList;
+/*
+ * Método m1
+	Acepta una cadena para extraer de ella todas las subcadenas formadas por dos 
+	o más caracteres repetidos y almacenarlas en un ArrayList.
+	Retorna el ArrayList.
+	
+	Método m2
+	 Hace lo mismo que el anterior con la ayuda de la 
+	 siguiente expresión regular: (.)\1+
+	 
+	Metodo m3a
+	 Acepta una matriz y retorna el valor máximo de los resultados obtenidos 
+	 al sumar los elementos de cada matriz interna de 3x3.
+	 La matriz no puede tener un número de filas o un número de columnas inferior a 3.
+	 En caso contrario retorna null.
+*/
+
+static void m1(String [] cadena)  {
+	ArrayList<String> lista = new ArrayList<>();
+	
+	lista.add("Tabla");
+	lista.add("Habla");
+	lista.add("Entabla");
+	
+}
+
+static void m2(String [] cadena) {
+	
+}
+
+static void m3a(int [] matriz) {
+	
+}
+
+static void m3b(int [] minas) {
+	int columna,fila,minas;
+	
+}
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+	 System.out.println();
+	}
 }
